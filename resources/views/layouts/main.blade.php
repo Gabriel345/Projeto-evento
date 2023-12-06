f9eb75b47e7c2e51aa9db8c8a037f71b49642a83
Gabriel345 <gabrielmzenerato242@gmail.com>
2023-12-06 18:36:09 -0300

    melhoras front

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,37 +15,36 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <div class="collapse navbar-collapse" id="navbar">
-                <a href="/" class="navbar-brand">Logo</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <nav class="navbar navbar-expand-sm bg-light navbar-dark fixed-top">
+            <div class="container-fluid navbar-container-fluid">
+                <a class="navbar-brand" href="/">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-
-
+                <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
+                        <li class="nav-item">
                             <a class="nav-link" href="/">Inicio</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/events/create">Criar Eventos</a>
                         </li>
                         @auth
-                        <li class="nav-item">
-                            <a class="nav-link" href="/dashboard">Meus Eventos</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/dashboard">Meus Eventos</a>
+                            </li>
                         @endauth
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="/login">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/register">Cadastrar</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/login">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/register">Cadastrar</a>
+                            </li>
                         @endguest
+                    </ul>
                 </div>
-                </ul>
             </div>
         </nav>
     </header>
@@ -53,7 +52,7 @@
         <div class="container-fluid">
             <div class="row">
                 @if (session('msg'))
-                <p class="msg">{{ session('msg') }}</p>
+                    <p class="msg">{{ session('msg') }}</p>
                 @endif
                 @yield('content')
             </div>
@@ -85,105 +84,100 @@
                         alt="Unigine logo">
                         </a> -->
 
-                        <p class="footer-text">
-                            Este é o nosso site do projeto integrador III se tudo der certo não repetiremos de ano.
-                            Deus seja conosco, amém.
-                        </p>
-                        
-                        <ul class="social-list">
-                            
-                            <li>
-                                <a href="#" class="social-link">
-                                    <ion-icon name="logo-facebook"></ion-icon>
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a href="#" class="social-link">
-                                    <ion-icon name="logo-twitter"></ion-icon>
-                                </a>
-                            </li>
+                    <p class="footer-text">
+                        Este é o nosso site do projeto integrador III se tudo der certo não repetiremos de ano.
+                        Deus seja conosco, amém.
+                    </p>
 
-                            <li>
-                                <a href="https://www.instagram.com/fatectq" target=_blank class="social-link">
-                                    <ion-icon name="logo-instagram"></ion-icon>
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a href="#" class="social-link">
-                                    <ion-icon name="logo-youtube"></ion-icon>
-                                </a>
-                            </li>
-                            
-                        </ul>
-                        
-                    </div>
+                    <ul class="social-list">
 
-                    <div class="footer-list">
+                        <li>
+                            <a href="#" class="social-link">
+                                <ion-icon name="logo-facebook"></ion-icon>
+                            </a>
+                        </li>
 
-                        <p class="title footer-list-title has-after"> Link Usuário </p>
-                        
-                        <ul>
-                            
-                            <li>
-                                <a href="#" class="footer-link"> Termos do Usuário </a>
-                            </li>
-                           
-                            <li>
-                                <a href="#" class="footer-link"> sei la  </a>
-                            </li>
-                            
-                            <li>
-                                <a href="#" class="footer-link"> sei la  </a>
-                            </li>
-                            
-                            <li>
-                                <a href="#" class="footer-link"> Contate-nos </a>
-                            </li>
-                            
-                        </ul>
-                        
-                    </div>
+                        <li>
+                            <a href="#" class="social-link">
+                                <ion-icon name="logo-twitter"></ion-icon>
+                            </a>
+                        </li>
 
-                    <div class="footer-list">
+                        <li>
+                            <a href="https://www.instagram.com/fatectq" target=_blank class="social-link">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a>
+                        </li>
 
-                        <p class="title footer-list-title has-after"> Contato </p>
-                        
-                        <div class="contact-item">
-                            <span class="span">Localização:</span>
-                            
-                            <address class="contact-link">
-                                Av. Dr. Flávio Henrique Lemos, 585 Portal Itamaracá,
-                                Taquaritinga/SP
-                            </address>
-                        </div>
-                        
-                        <div class="contact-item">
-                            <span class="span"> Fale conosco: </span>
-                            
-                            <a href="mailto:Info@gamehive.com" class="contact-link"> Info@EB-events.com </a>
-                        </div>
+                        <li>
+                            <a href="#" class="social-link">
+                                <ion-icon name="logo-youtube"></ion-icon>
+                            </a>
+                        </li>
 
-                        <div class="contact-item">
-                            <span class="span"> Telefone: </span>
-                            
-                            <a href="tel:+12345678910" class="contact-link"> </a> (16) 4002-8922 </a>
-                        </div>
+                    </ul>
 
-                    </div>
-
-                    
                 </div>
-            </div>
 
-            <div class="footer-bottom">
+                <div class="footer-list">
+
+                    <p class="title footer-list-title has-after"> Link Usuário </p>
+
+                    <ul>
+
+                        <li>
+                            <a href="#" class="footer-link"> Termos do Usuário </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="footer-link"> sei la </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="footer-link"> sei la </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="footer-link"> Contate-nos </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <div class="footer-list">
+
+                    <p class="title footer-list-title has-after"> Contato </p>
+
+                    <div class="contact-item">
+                        <span class="span">Localização:</span>
+
+                        <address class="contact-link">
+                            Av. Dr. Flávio Henrique Lemos, 585 Portal Itamaracá,
+                            Taquaritinga/SP
+                        </address>
+                    </div>
+
+                    <div class="contact-item">
+                        <span class="span"> Fale conosco: </span>
+
+                        <a href="mailto:Info@gamehive.com" class="contact-link"> Info@EB-events.com </a>
+                    </div>
+
+                    <div class="contact-item">
+                        <span class="span"> Telefone: </span>
+
+                        <a href="tel:+12345678910" class="contact-link"> </a> (16) 4002-8922 </a>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="footer-bottom">
             <div class="container-bottom">
-                
-                <p class="copyright">
-                    &copy; 2023 EB events só gente boa.
-                </p>
-                
             </div>
         </div>
 
